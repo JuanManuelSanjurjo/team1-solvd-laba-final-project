eb695a7bffe51c2e3f9df8a78e89190ca7a0ad42
Matheus Antunes <mta.santos@gmail.com>
2025-07-16 18:12:42 -0400

    fix: mui setup

---
+++ src/utils/theme.ts
@@ -0,0 +1,16 @@
+"use client";
+
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#FE645E",
+    },
+  },
+  typography: {
+    fontFamily: "var(--font-work-sans)",
+  },
+});
+
+export default theme;
