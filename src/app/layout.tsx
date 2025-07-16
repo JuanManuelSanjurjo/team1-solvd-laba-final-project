3d108bd159cb0cd1aec9c806d9da6b0d93998131
Matheus Antunes <mta.santos@gmail.com>
2025-07-15 22:49:05 -0400

    fix: remove modal structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,19 +20,14 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-  modal,
 }: Readonly<{
   children: React.ReactNode;
-  modal: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <ReactQueryProvider>
-          <main>
-            {children}
-            {modal}
-          </main>
+          <main>{children}</main>
         </ReactQueryProvider>
       </body>
     </html>
--- src/app/my-products/@modal/(.)my-products/[product-id]/edit/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function EditProductModal() {
-  return <h1>Edit Product Modal</h1>;
-}
