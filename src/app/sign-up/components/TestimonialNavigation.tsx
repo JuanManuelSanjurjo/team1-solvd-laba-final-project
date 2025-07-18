6b881cec2ab90fbc6f52dd9b85c431b5824d29d4
Matheus Antunes <mta.santos@gmail.com>
2025-07-17 22:52:08 -0400

    fix: declaring component inside another component

--- src/app/sign-up/components/TestimonialNavigation.tsx
+++ src/app/sign-up/components/TestimonialNavigation.tsx
@@ -3,15 +3,15 @@
 import { Box, styled } from "@mui/material";
 import { ArrowLeft2, ArrowRight2 } from "iconsax-react";
 
-export default function TestimonialNavigation() {
-  const StyledBox = styled(Box)({
-    width: "38px",
-    height: "38px",
-    border: "2px solid rgba(228, 228, 228, 0.78)",
-    borderRadius: "100%",
-    cursor: "pointer",
-  });
+const StyledBox = styled(Box)({
+  width: "38px",
+  height: "38px",
+  border: "2px solid rgba(228, 228, 228, 0.78)",
+  borderRadius: "100%",
+  cursor: "pointer",
+});
 
+export default function TestimonialNavigation() {
   return (
     <Box sx={{ display: "flex", gap: "32px" }}>
       <StyledBox>
