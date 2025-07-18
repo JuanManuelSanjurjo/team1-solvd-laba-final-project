723780da77ee9b6fa2066588e78e1448cf50d2a4
Matheus Antunes <mta.santos@gmail.com>
2025-07-17 22:20:30 -0400

    docs: remove unnecessary comments at fonts.ts

--- src/utils/fonts.ts
+++ src/utils/fonts.ts
@@ -1,9 +1,8 @@
-// src/app/fonts.ts
-import { Work_Sans } from 'next/font/google';
+import { Work_Sans } from "next/font/google";
 
 export const worksans = Work_Sans({
-  subsets: ['latin'],
-  variable: '--font-worksans',
-  weight: ['300', '400', '500', '600', '700'],
-  display: 'swap',
+  subsets: ["latin"],
+  variable: "--font-worksans",
+  weight: ["300", "400", "500", "600", "700"],
+  display: "swap",
 });
