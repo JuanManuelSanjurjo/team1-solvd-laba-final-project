a5d6e9fdc9c4fc3ae5284b3a2709e81af6e7a6a7
Giuliana Iemma <giuliana.iemma@gmail.com>
2025-07-22 21:45:51 -0300

    Work in progress

---
+++ src/components/Buttons/ProductQuantityButton.tsx
@@ -0,0 +1,12 @@
+import { IconButton as MuiIconButton, IconButtonProps as MuiIconButtonProps} from "@mui/material";
+
+type IconButtonProps = {
+    type : "Add" | "-";
+}
+
+export default IconButton () {
+    
+    return(
+
+    )
+}
