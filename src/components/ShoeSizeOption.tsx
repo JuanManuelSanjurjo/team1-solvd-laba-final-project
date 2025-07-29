4810d1e389d6297b2c8d9bc3796af6a79a67c8f7
JuanManuelSanjurjo <juanmanuelsanjurjo@gmail.com>
2025-07-29 17:40:53 -0300

    minor transition added

--- src/components/ShoeSizeOption.tsx
+++ src/components/ShoeSizeOption.tsx
@@ -1,24 +1,24 @@
-"use client"
+"use client";
 
-import { Box, Checkbox, FormControl } from "@mui/material"
-import { useState } from "react"
+import { Box, Checkbox, FormControl } from "@mui/material";
+import { useState } from "react";
 
 /**
- * This component represents options of shoe sizes. It is built to be used as a checkbox, with the possibility of choosing more than one option at the same time. 
- * 
+ * This component represents options of shoe sizes. It is built to be used as a checkbox, with the possibility of choosing more than one option at the same time.
+ *
  * @component
  * @param {ShoeSizeOptionProps} props - Props for the component
- * @param {number} props.size - String that represents the size. 
+ * @param {number} props.size - String that represents the size.
  * @returns {JSX.Element} The styled checkbox component
- * 
+ *
  * @example
  * <ShoeSizeOption size={40} />
  */
 
 type ShoeSizeOptionProps = {
-    size: number
-    disabled: boolean
-}
+  size: number;
+  disabled: boolean;
+};
 
 const baseStyles = {
   borderRadius: "8px",
@@ -26,52 +26,62 @@ const baseStyles = {
   textAlign: "center",
   cursor: "pointer",
   border: "1px solid #494949",
-}
+};
 
 const stateStyles = {
-    default: {
-        color: "#5c5c5c",
-        backgroundColor: "#ffffff",
-        cursor: "pointer"
+  default: {
+    color: "#5c5c5c",
+    backgroundColor: "#ffffff",
+    cursor: "pointer",
+  },
+  checked: {
+    border: "1px solid #FE645E",
+    color: "#FE645E",
+  },
+  disabled: {
+    color: "#C3C3C3",
+    backgroundColor: "#F0F0F0",
+    cursor: "not-allowed",
+  },
+};
 
-    },
-    checked: {
-        border: "1px solid #FE645E",
-        color: "#FE645E"
-    },
-    disabled: {
-        color: "#C3C3C3",
-        backgroundColor: "#F0F0F0",
-        cursor: "not-allowed",
+export default function ShoeSizeOption({
+  size,
+  disabled,
+}: ShoeSizeOptionProps) {
+  const [checked, setChecked] = useState(false);
 
-    }
-}
+  const getState = () => {
+    if (disabled) return "disabled";
+    if (checked) return "checked";
+    return "default";
+  };
 
-export default function ShoeSizeOption ({size, disabled} : ShoeSizeOptionProps) {
-    const [checked, setChecked] = useState(false);
-    
-    const getState = () => {
-    if (disabled) return "disabled"
-    if (checked) return "checked"
-    return "default"
-  }
+  const state = getState();
 
-    const state = getState()
-
-  
-    return (
+  return (
     <Box>
-        <FormControl>
-            <Checkbox sx={{display: "none"}} checked={checked} onChange={(e)=> setChecked(e.target.checked)} disabled={disabled}/>
+      <FormControl>
+        <Checkbox
+          sx={{ display: "none" }}
+          checked={checked}
+          onChange={(e) => setChecked(e.target.checked)}
+          disabled={disabled}
+        />
 
-            <Box   sx={{
+        <Box
+          sx={{
             ...baseStyles,
             ...stateStyles[state],
-            }}  
-
-            onClick={() => {if (!disabled) setChecked(!checked)}}> 
-                EU-{size}
-            </Box>
-        </FormControl>
-    </Box>)
+            transition: "all 0.2s ease-in",
+          }}
+          onClick={() => {
+            if (!disabled) setChecked(!checked);
+          }}
+        >
+          EU-{size}
+        </Box>
+      </FormControl>
+    </Box>
+  );
 }
