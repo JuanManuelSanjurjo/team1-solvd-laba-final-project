a200afe52a189f470453eaa88800413569c8c8ac
FranciscoAbad <unlafranciscoabad@gmail.com>
2025-07-30 17:48:13 -0300

    Fix: update hasActiveFilters to recive ProductFilters type

--- src/lib/filterUtils.ts
+++ src/lib/filterUtils.ts
@@ -1,3 +1,5 @@
+import { ProductFilters } from "./getFiltersFromSearchParams";
+
 /**
  * Determines whether any filters are currently active.
  *
@@ -5,13 +7,11 @@
  * - Its value is a non-empty array.
  * - Its value is a truthy primitive (e.g., non-empty string, number, boolean `true`, etc.).
  *
- * @param {Record<string, any>} filters - An object representing filter values where keys are filter names.
+ * @param {ProductFilters} filters - An object representing filter values where keys are filter names.
  * @returns {boolean} - Returns `true` if at least one filter is active, otherwise `false`.
  */
 
-export const hasActiveFilters = (
-  filters: Record<string, string | {} | []>
-): boolean => {
+export const hasActiveFilters = (filters: ProductFilters): boolean => {
   return Object.values(filters).some((value) =>
     Array.isArray(value) ? value.length > 0 : !!value
   );
