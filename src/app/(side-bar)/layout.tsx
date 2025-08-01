29c3cb74332c76e255fd7b5ee670065f4aa93c7d
JuanManuelSanjurjo <juanmanuelsanjurjo@gmail.com>
2025-08-01 11:29:13 -0300

    Refactor: grouped logged-in pages with sidebar into a layout group

--- src/app/(side-bar)/layout.tsx
+++ src/app/(side-bar)/layout.tsx
@@ -1,25 +1,21 @@
 import { Box } from "@mui/material";
-import MyProductsBanner from "./components/MyProductsBanner";
-import MyProductsMainContent from "./components/MyProductsMainContent";
 import AuthenticatedSidebar from "@/components/AuthenticatedSidebar";
 
 /**
- * MyProducts
+ * Layout
  *
- * This component renders the main content of the My Products page.
- * It includes a banner, a main content section, and a sidebar.
+ * This component renders the layout for the group of pages with the logged-in user sidebar.
  *
- * @component
- *
- * @returns {JSX.Element} The main content of the My Products page.
+ * @returns {JSX.Element} The main content of the page.
  */
-export default function MyProductsPage() {
+
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <Box
       sx={{
         display: "flex",
         gap: 2,
-        marginBlock: { xs: 10, md: 20 },
+        marginBlock: { xs: 10, md: 18 },
         marginInline: { lg: "40px" },
       }}
     >
@@ -48,8 +44,7 @@ export default function MyProductsPage() {
         }}
         width={"100%"}
       >
-        <MyProductsBanner />
-        <MyProductsMainContent />
+        {children}
       </Box>
     </Box>
   );
---
+++ src/app/(side-bar)/my-wishlist/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+import CardContainer from "@/components/cards/CardContainer";
+import Card from "@/components/cards/Card";
+import { Box, Typography } from "@mui/material";
+import { useWishlistStore } from "@/store/wishlist";
+
+export default function MyWishlist() {
+  const wishList = useWishlistStore((state) => state.wishList);
+
+  return (
+    <>
+      <Box display="flex" alignItems="center">
+        <Typography
+          variant="h2"
+          sx={{
+            marginTop: "40px",
+            marginLeft: {
+              xs: "24px",
+              lg: 0,
+            },
+          }}
+        >
+          My Wishlist
+        </Typography>
+      </Box>
+      <CardContainer>
+        {wishList.map((product, index) => (
+          <Card product={product} key={index} topAction="cardButtonWishList" />
+        ))}
+      </CardContainer>
+    </>
+  );
+}
