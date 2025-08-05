a59529d313ad670b8c452da9422a05d0589f536e
JuanManuelSanjurjo <juanmanuelsanjurjo@gmail.com>
2025-08-05 18:10:30 -0300

    Add: added empty state card for my producs, wishlist, etc

---
+++ src/components/MyProductsEmptyState.tsx
@@ -0,0 +1,62 @@
+import { Bag } from "iconsax-react";
+import { Box, Typography, Button } from "@mui/material";
+
+type MyProductsEmptyStateProps = {
+  title: string;
+  subtitle: string;
+  onClick: () => void;
+};
+
+/**
+ * MyProductsEmptyState
+ *
+ * This component renders an empty state for the My Products page.
+ * It displays a bag icon, a message, and a button to add a product.
+ *
+ * @component
+ *
+ * @returns {JSX.Element} An empty state for the My Products page.
+ */
+export default function MyProductsEmptyState({
+  title,
+  subtitle,
+  onClick,
+}: MyProductsEmptyStateProps) {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 4,
+        height: "100%",
+      }}
+    >
+      <Bag size={20} color="#292d32" />
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: 1,
+        }}
+      >
+        <Typography
+          variant="subtitle1"
+          fontWeight={500}
+          sx={{ fontSize: { xs: 16, md: 20 } }}
+        >
+          {title}
+        </Typography>
+        <Typography variant="body2" sx={{ fontSize: { xs: 12, md: 15 } }}>
+          {subtitle}
+        </Typography>
+      </Box>
+      <Button variant="contained" onClick={onClick}>
+        Add Product
+      </Button>
+    </Box>
+  );
+}
