f8dab0820a585c9a27cdb2c32c5deda70bd1783e
JuanManuelSanjurjo <juanmanuelsanjurjo@gmail.com>
2025-08-13 21:44:20 -0300

    Moved useMediaBreakpoints to shared hooks folder

--- src/hooks/useMediaBreakpoints.tsx
+++ src/hooks/useMediaBreakpoints.tsx
@@ -5,11 +5,13 @@ import { useMediaQuery, useTheme } from "@mui/material";
  *
  * This hook is used to determine the current screen size.
  * It returns an object with boolean values for isMobile, isTablet, and isDesktop.
- *
- * @returns {Object} An object with boolean values for isMobile, isTablet, and isDesktop.
  */
 
-export default function useMediaBreakpoints() {
+export default function useMediaBreakpoints(): {
+  isMobile: boolean;
+  isTablet: boolean;
+  isDesktop: boolean;
+} {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
   const isTablet = useMediaQuery(theme.breakpoints.between("sm", "md"));
