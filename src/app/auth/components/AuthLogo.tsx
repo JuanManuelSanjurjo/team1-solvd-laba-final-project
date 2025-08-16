89ec8c0706ac3924ce925032751712ae00207b08
Matheus Antunes <mta.santos@gmail.com>
2025-08-16 13:55:03 -0400

    add: link to homepage on logo in auth pages

--- src/app/auth/components/AuthLogo.tsx
+++ src/app/auth/components/AuthLogo.tsx
@@ -1,15 +1,18 @@
 import { Box } from "@mui/material";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function AuthLogo() {
   return (
     <Box sx={{ position: "absolute", top: "50px", left: "40px" }}>
-      <Image
-        src="/assets/logo/logo-black.svg"
-        alt="Shoes Shop Logo"
-        width={40}
-        height={30}
-      />
+      <Link href="/">
+        <Image
+          src="/assets/logo/logo-black.svg"
+          alt="Shoes Shop Logo"
+          width={40}
+          height={30}
+        />
+      </Link>
     </Box>
   );
 }
