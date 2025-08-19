a240a0b478bb86ee71ad95f843547a7e23baa958
Juan Manuel Sanjurjo <57844658+JuanManuelSanjurjo@users.noreply.github.com>
2025-08-19 19:28:04 -0300

    refactor: changed usedDeferred to a use debounce to reduce api calls (#176)
    * refactor: changed usedDeferred to a use debounce to minimize api calls
    
    * Enhancement: addes stale time to query

--- src/lib/strapi/fetchUserProducts.ts
+++ src/lib/strapi/fetchUserProducts.ts
@@ -7,11 +7,10 @@ export async function fetchUserProducts(userId: number, token: string) {
         "Content-Type": "application/json",
         Authorization: `Bearer ${token}`,
       },
-    }
+    },
   );
 
   if (!response.ok) throw new Error("Network response was not ok");
   const json = await response.json();
-  console.log(json.products);
   return json.products;
 }
