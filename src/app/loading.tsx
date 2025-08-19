5b8c29e29ff259214822745e5e973ac1d9bb33b8
Matheus Antunes <mta.santos@gmail.com>
2025-08-18 23:09:16 -0400

    add: basic loading tsx

---
+++ src/app/loading.tsx
@@ -0,0 +1,39 @@
+import { Box } from "@mui/material";
+import Image from "next/image";
+import { JSX } from "react";
+
+export default function Loading(): JSX.Element {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
+      <Box
+        sx={{
+          animation: "logoFade 2s ease-in-out infinite",
+          "@keyframes logoFade": {
+            "0%, 100%": {
+              opacity: 0.3,
+            },
+            "50%": {
+              opacity: 1,
+            },
+          },
+        }}
+      >
+        <Image
+          src="/assets/logo/logo-orange.svg"
+          alt="Loading..."
+          width={120}
+          height={120}
+          priority
+        />
+      </Box>
+    </Box>
+  );
+}
