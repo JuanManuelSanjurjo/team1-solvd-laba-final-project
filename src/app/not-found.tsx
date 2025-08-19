d6b1c39eef711a8930215e9452316ae7f1e93dab
JuanManuelSanjurjo <juanmanuelsanjurjo@gmail.com>
2025-08-19 09:40:54 -0300

    Fix: error pages rework
    Go back send user to previous page. Extranction of component to a
    reusable one

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,7 @@
 "use client";
-import { Typography, Button, Box } from "@mui/material";
-import Link from "next/link";
+import { Box } from "@mui/material";
 import { JSX } from "react";
+import ErrorPageFloatingContent from "@/components/errors/ErrorPageFloatingContent";
 
 export default function NotFound(): JSX.Element {
   return (
@@ -53,75 +53,7 @@ export default function NotFound(): JSX.Element {
         }}
       />
       {/* Floating content */}
-      <FloatingContent />
-    </Box>
-  );
-}
-
-function FloatingContent(): JSX.Element {
-  return (
-    <Box
-      position="absolute"
-      left={{
-        xs: "50%",
-        md: "8%",
-      }}
-      height="100%"
-      padding="40px 16px"
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        height: "100%",
-        textAlign: {
-          xs: "center",
-          md: "start",
-        },
-        justifyContent: {
-          xs: "space-between",
-          md: "center",
-        },
-        transform: {
-          xs: "translateX(-50%)",
-          md: "translateX(0%)",
-        },
-      }}
-    >
-      <Box maxWidth={{ xs: 320, md: "45%" }}>
-        <Typography variant="h2" fontWeight={500} fontSize={{ xs: 30, md: 45 }}>
-          Error 404
-        </Typography>
-        <Typography
-          variant="body1"
-          color="text.secondary"
-          sx={{ marginTop: "20px" }}
-          fontSize={{ xs: 12, md: 20 }}
-          mb={2}
-        >
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
-          nonummy nibh euismod tincidunt ut laoreet dolore magna
-        </Typography>
-      </Box>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          marginTop: "20px",
-          width: 320,
-          gap: 2,
-          "& .MuiButton-root": {
-            width: "100%",
-            fontSize: 16,
-            height: 40,
-          },
-        }}
-      >
-        <Link href="/" style={{ width: "100%" }}>
-          <Button variant="outlined">Go back</Button>
-        </Link>
-        <Link href="/" style={{ width: "inherit" }}>
-          <Button variant="contained">Home</Button>
-        </Link>
-      </Box>
+      <ErrorPageFloatingContent />
     </Box>
   );
 }
