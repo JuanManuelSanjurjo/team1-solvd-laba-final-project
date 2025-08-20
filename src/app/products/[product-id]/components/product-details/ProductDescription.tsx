e65ebc962bb1a68b7716371cff84c5f7e9294fe8
JuanManuelSanjurjo <juanmanuelsanjurjo@gmail.com>
2025-08-20 19:40:19 -0300

    Fix: #184 edge case - overflowing text on product detail name, color and description

--- src/app/products/[product-id]/components/product-details/ProductDescription.tsx
+++ src/app/products/[product-id]/components/product-details/ProductDescription.tsx
@@ -27,6 +27,7 @@ export default function ProductDescription({
         color="text.secondary"
         sx={{
           lineHeight: "24px",
+          wordBreak: "break-word",
           fontSize: { xs: "14px", sm: "16px", md: "16px" },
         }}
       >
