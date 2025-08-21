10936dae409190426be1a76d0960126be929e1e6
JuanManuelSanjurjo <juanmanuelsanjurjo@gmail.com>
2025-08-20 21:27:28 -0300

    added error throwing in get product details fetch function

--- src/lib/actions/get-product-details.ts
+++ src/lib/actions/get-product-details.ts
@@ -14,8 +14,11 @@ export async function getProductDetails(id: string) {
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_API_URL}/products/${id}?fields[0]=name&fields[1]=price&fields[2]=description
       &populate[color][fields][0]=name&populate[sizes][fields][0]=value&populate[images][fields][0]=url&populate[gender][fields][0]=name`,
-      { cache: "no-store" }
+      { cache: "no-store" },
     );
+    if (!response.ok) {
+      throw new Error("Failed to fetch product details");
+    }
     const responseData = await response.json();
     return responseData || { data: null };
   } catch (error) {
